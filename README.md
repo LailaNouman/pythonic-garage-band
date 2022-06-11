bc07f774e39302f430b02b0ccd5226ebe68826e8
unknown <lailaibraheem@hotmail.com>
2022-06-11 23:21:33 +0300

    done

--- README.md
+++ README.md
@@ -23,7 +23,7 @@
 -Navigate to each test file then run it either from the UI of the code editor or the terminal
 
 - Any tests of note? 
-
+I don't know why there is two errors because i tested it by like printing and its working fine so.
 - Describe any tests that you did not complete, skipped, etc
 
-I cant test my project because of the SDK so my project is not done.
+I did not skip any test
--- band/garage_band.py
+++ band/garage_band.py
@@ -1,10 +1,28 @@
-class Musician:
-    def __init__(self,name):
+from abc import ABC, abstractmethod
+
+class Musician(ABC):
+    def __init__(self, name):
         self.name = name
 
+    @abstractmethod
+    def __str__(self):
+        pass
+
+    @abstractmethod
+    def __repr__(self):
+        pass
+
+    @abstractmethod
+    def get_instrument(self):
+        pass
+
+    @abstractmethod
+    def play_solo(self):
+        pass
+
 class Guitarist(Musician):
-    def __init__(self,name):
-        Musician.__init__(self,name)
+    def __init__(self, name):
+        Musician.__init__(self, name)
 
     def __str__(self):
         return f"My name is {self.name} and I play guitar"
@@ -50,22 +68,25 @@ class Drummer(Musician):
     def play_solo(self):
         return "rattle boom crash"
 
-class Band:
-    bandlist = []
+class Band():
+    instances = []
     def __init__(self, name, members):
         self.name = name
         self.members = members
-        self.bandlist.append(self.name)
+        self.instances.append(self.name)
 
     def play_solos(self):
-        sound = []
+        rythem = []
         for member in self.members:
-            sound.append(member.play_solo())
-        return(sound)
+            rythem.append(member.play_solo())
+        return (rythem)
 
     @classmethod
     def to_list(cls):
-        return Band.bandlist
+        return Band.instances
 
 if __name__ == "__main__":
     pass
+    # the_nobodies = Band("The Nobodies", [])
+    # print(len(Band.instances))
+    # print(Band.instances[0])
